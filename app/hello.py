6457d41c610d61756fe9d6fbd55b5ecc8abc0e0a
wmr <wmr101@gmail.com>
2017-08-24 09:28:03 +0200

    Flask api.

--- app/hello.py
+++ app/hello.py
@@ -4,9 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return render_template('hello.html')
+    return render_template('index.html')
+
+@app.route('/submit/')
+def submit():
+    return "valami"
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
